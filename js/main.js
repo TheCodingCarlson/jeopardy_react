0715e8a91b057c072c8b423dd4a67413e76b297b
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2016-02-17 17:48:54 -0800

    adding basic react component

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var MyApp = React.createClass({
+	render: function() {
+		return (
+			<div className='well'>
+				<h1>Hello World</h1>
+			</div>
+		);
+	}
+});
+
+
+
+ReactDOM.render(<MyApp />, document.getElementById('sportsApp'));
