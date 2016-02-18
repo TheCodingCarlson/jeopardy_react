a09e41024d0bc1451fd13df81e9d0a34902c8eb9
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2016-02-17 20:42:09 -0800

    adding result list react class

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
-$.get('http://jservice.io/api/clues', function(data) {
-	for(var i = 0; i < data.length; i++) {
-		if(data[i].question !== '') {
-			console.log(data[i].question);
-		}
-	}
-});
+// function getData() {
+// 	$.get('http://jservice.io/api/random?count=100', function(data) {
+// 		var info = [];
+// 		for(var i = 0; i < data.length; i++) {
+// 			var obj = {'question': data[i].question, 'answer': data[i].answer};
+// 			info.push(obj);	
+// 		}
+// 		console.log(info);
+// 	});
+// }
 
 
 var MyApp = React.createClass({
+	getData: function() {
+		$.get('http://jservice.io/api/random?count=100', function(data) {
+			var info = [];
+			for(var i = 0; i < data.length; i++) {
+				var obj = {'question': data[i].question, 'answer': data[i].answer};
+				info.push(obj);	
+			}
+			return info;
+		});
+	},
 	render: function() {
 		return (
 			<div>
 				<h1>Jeopardy</h1>
+				<ResultList data={this.getData}
 			</div>
 		);
+	}
+});
+
+var ResultList = Reach.createClass({
+	render: function() {
+		
 	}
 });
 
