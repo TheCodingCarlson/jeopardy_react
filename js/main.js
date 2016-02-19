4705ce900f912e7312f66337939786cbbf0a79f0
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2016-02-19 11:02:15 -0800

    experimenting with ajax in react/component layout

--- js/main.js
+++ js/main.js
@@ -1,42 +1,64 @@
 // function getData() {
-// 	$.get('http://jservice.io/api/random?count=100', function(data) {
-// 		var info = [];
-// 		for(var i = 0; i < data.length; i++) {
-// 			var obj = {'question': data[i].question, 'answer': data[i].answer};
-// 			info.push(obj);	
-// 		}
-// 		console.log(info);
-// 	});
+	var info = [];
+	$.get('http://jservice.io/api/random?count=100', function(data) {
+		for(var i = 0; i < data.length; i++) {
+			var obj = {'question': data[i].question, 'answer': data[i].answer};
+			info.push(obj);	
+		}
+		console.log(info);
+	});
+	
 // }
 
 
 var MyApp = React.createClass({
-	getData: function() {
-		$.get('http://jservice.io/api/random?count=100', function(data) {
-			var info = [];
-			for(var i = 0; i < data.length; i++) {
-				var obj = {'question': data[i].question, 'answer': data[i].answer};
-				info.push(obj);	
-			}
-			return info;
-		});
-	},
 	render: function() {
 		return (
 			<div>
 				<h1>Jeopardy</h1>
-				<ResultList data={this.getData}
+				<ResultsList />
 			</div>
 		);
 	}
 });
 
-var ResultList = Reach.createClass({
+var ResultsList = React.createClass({
+	getInitialState: function() {
+		return {
+			question: '',
+			answer: ''
+		};
+	},
+	componentDidMount: function() {
+		this.serverRequest = $.get('http://jservice.io/api/random?count=100', function(data) {
+			var first = data[0];
+			this.setState({
+				question: first.question,
+				answer: first.answer
+			});
+		}.bind(this));
+	},
 	render: function() {
-		
+		return (
+			<div>
+				{this.state.question}
+				{this.state.answer}
+			</div>
+		);
 	}
+
 });
 
+var Questions = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<p>One Para</p>
+				<p>Two Para</p>
+			</div>
+		);
+	}
+});
 
 
 ReactDOM.render(<MyApp />, document.getElementById('sportsApp'));
