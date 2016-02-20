f9a1bcfc2762e4d010f56e392a153feb2dc848f3
Christian Carlson <chris.carlson.dev@gmail.com>
2016-02-19 17:44:17 -0800

    adding hammer.js/playing around with adding it to react

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
-// function getData() {
-	var info = [];
-	$.get('http://jservice.io/api/random?count=100', function(data) {
-		for(var i = 0; i < data.length; i++) {
-			var obj = {'question': data[i].question, 'answer': data[i].answer};
-			info.push(obj);	
-		}
-		console.log(info);
-	});
-	
-// }
-
-
 var MyApp = React.createClass({
 	render: function() {
 		return (
 			<div>
 				<h1>Jeopardy</h1>
-				<ResultsList />
+				<ResultsList />		
 			</div>
 		);
 	}
@@ -31,9 +18,11 @@ var ResultsList = React.createClass({
 	componentDidMount: function() {
 		var info = [];
 		this.serverRequest = $.get('http://jservice.io/api/random?count=100', function(data) {
-			for(var i = 0; i < 5; i++) {
-				var question = [data[i].question, data[i].answer];
-				info.push(question);
+			for(var i = 0; i < data.length; i++) {
+				if(data[i].question && data[i].answer) {
+					var question = [data[i].question, data[i].answer];
+					info.push(question);
+				}
 			}	
 			this.setState({
 				trivia: info
@@ -43,7 +32,7 @@ var ResultsList = React.createClass({
 	render: function() {
 		var questions = this.state.trivia.map(function(question, idx) {
 			return <Question key={idx} data={question} />
-		})
+		});
 		return (
 			<div>
 				{questions}
@@ -53,12 +42,17 @@ var ResultsList = React.createClass({
 });
 
 var Question = React.createClass({
+	componentDidMount: function() {
+		this.hammer = Hammer(this.getDOMNode());
+		this.hammer.on('swipeleft', function() {
+			console.log('worked');
+		});
+	},
 	render: function() {
 		var question = this.props.data;
 		return (
 			<div className='well'>
-				<p>{question[0]}</p>
-				<p>{question[1]}</p>
+				<h2>{question[0]}</h2>
 			</div>
 		);
 	}
