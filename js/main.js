f94bcf528ede5cc0d464fd7b05232b08d039f991
Christian Carlson <chris.carlson.dev@gmail.com>
2016-03-16 16:53:46 -0700

    changed results list components

--- js/main.js
+++ js/main.js
@@ -2,12 +2,8 @@ var MyApp = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<img id='logo'  className='img-responsive' src='assets/logo.png' />	
-				<Infinite containerHeight={window.innerHeight} elementHeight={450}>
-					<ResultsList />
-					<ResultsList />
-					<ResultsList />
-				</Infinite>
+				<img id='logo'  className='img-responsive' src='assets/logo.png' />
+				<ResultsList />
 			</div>
 		);
 	}
@@ -17,48 +13,69 @@ var ResultsList = React.createClass({
 	getInitialState: function() {
 		return {
 			trivia: [],
-			message: 'Fetching Trivia...'
+			isInfiniteLoading: false
 		}
 	},
-	reset: function() {
-		location.reload();
+	getData: function(callback) {
+		$.get('http://jservice.io/api/random?count=10', callback);	
 	},
-	getData: function() {
-		var info = [];
-		this.serverRequest = $.get('http://jservice.io/api/random?count=10', function(data) {
-			for(var i = 0; i < data.length; i++) {
-				if(data[i].question && data[i].answer) {
-					var question = [data[i].question, data[i].answer];
-					info.push(question);
-				}
-			}	
-			this.setState({
-				trivia: info,
-				message: ''
-			});
-		}.bind(this));
+	buildElements: function(data) {
+		var newQuestions = [];
+		var count = this.state.trivia.length;
+		for(var i = 0; i < data.length; i++) {
+			if(data[i].question && data[i].answer) {
+				count++;
+				var question = [data[i].question, data[i].answer];
+				newQuestions.push(<Question key={count} data={question}/>);
+			}
+		}
+		
+		this.setState({
+			isInfiniteLoading: false,
+			trivia: this.state.trivia.concat(newQuestions)
+		})
 	},
-	componentWillMount: function() {
-		this.getData();
+	getMoreData: function() {
+		console.log('infinite load');
+		var that = this;
+		this.setState({
+			isInfiniteLoading: true
+		});
+		setTimeout(function() {
+			that.getData(that.buildElements);
+		}, 2500);
+
+		
+	},
+	questionInfiniteLoad: function() {
+		return <div className='infinite-list-item'><h1>Loading...</h1></div>;
 	},
 	render: function() {
-		var questions = this.state.trivia.map(function(question, idx) {
-			return <Question key={idx} data={question} />
-		});
 		return (
 			<div>
-				<h2>{this.state.message}</h2>
-				{questions}
-				<button className='btn btn-default btn-lg' onClick={this.reset}>Reset</button>
+				<Infinite useWindowAsScrollContainer 
+				elementHeight={300} 
+				infiniteLoadBeginEdgeOffset={100}
+				onInfiniteLoad={this.getMoreData}
+				loadingSpinnerDelegate={this.questionInfiniteLoad()}
+				isInfiniteLoading={this.state.isInfiniteLoading}
+                timeScrollStateLastsForAfterUserScrolls={0}
+                >
+				{this.state.trivia}
+				</Infinite>
 			</div>
 		);
 	}
 });
 
 var Question = React.createClass({
+	getDefaultProps: function() {
+		return {
+			height: 300
+		}
+	},
 	componentDidMount: function() {
 		window.mySwipe = Swipe(document.getElementById(this._reactInternalInstance._rootNodeID));
-
 	},
 	render: function() {
 		var question = this.props.data;
@@ -66,12 +83,12 @@ var Question = React.createClass({
 		return (
 			<div id={id} className='swipe'>
 				<div className='swipe-wrap'>
-					<div className='well'>
+					<div className='well' style={{ height: this.props.height }}>
 						<span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 						<h3>{question[0]}</h3>
 						<span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 					</div>
-					<div className='well'>
+					<div className='well' style={{ height: this.props.height }}>
 						<span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 						<h3>{question[1]}</h3>
 						<span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
